a7aa3fee39cbbe9ca7be315e7af3e868d4b724ba
kevinjohannesson <kroon.kevin@gmail.com>
2020-12-14 14:23:50 +0100

    removed unneccesary packages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router,  Switch, Route } from "react-router-dom";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import BarLayout from "./layouts/Bar";
 import FooLayout from "./layouts/Foo";
+import { LayoutName } from "./layouts/types";
 
 // import { createMemoryHistory } from 'history';
 // import { ConnectedRouter } from "connected-react-router";
@@ -26,27 +27,26 @@ const barLayoutEndpoints = [
   },
 ];
 
+interface Endpoints {
+    url: string,
+    component: () => JSX.Element,
+    layout: LayoutName,
+}
+
 export default function App() {
-  // console.log("App()");
 
   const currentRouteRef = useRef<string | undefined>();
-
   const targetRouteRef = useRef<string | undefined>();
-
-  // useselector(getlayoutview)
-  // const history = createMemoryHistory();
+  
   return (
-    // <ConnectedRouter history={history}>
     <Router>
       <div className="wrapper">
         <Route
           render={({ location }) => {
-            // console.log("route render");
-            // console.log(location.pathname);
+            
             targetRouteRef.current = location.pathname;
-            // const targetLocation = location;
+            
             return (
-              // <AnimatePresence initial={true} >
               <AnimatePresence initial={true} exitBeforeEnter>
                 <Switch location={location} key={location.pathname}>
                   <Route
@@ -81,7 +81,6 @@ export default function App() {
                       currentRouteRef.current = location.pathname;
                       return (
                         <Switch>
-                          {/* <BarLayout animateIn={true} animateOut={true}> */}
                           <BarLayout animateIn={shouldAnimateIn} animateOut={shouldAnimateOut}>
                             {barLayoutEndpoints.map((endpoint) => (
                               <Route exact path={endpoint.url} component={endpoint.component} key={endpoint.url} />
@@ -96,77 +95,10 @@ export default function App() {
             );
           }}
         />
-
-        {/* <Route
-          render={({ location }) => (
-            <AnimatePresence initial={true} exitBeforeEnter>
-              <Switch location={location} key={location.pathname}>
-                <Route exact path={["/foo"]}>
-                  <Switch >
-                    <FooLayout>
-                      <Route exact path="/foo" component={Foo} />
-                    </FooLayout>
-                  </Switch>
-                </Route>
-
-                <Route exact path={["/bar", "/baz"]}>
-                  <Switch>
-                    <BarLayout>
-                      <Route exact path="/bar" component={Bar} />
-                      <Route exact path="/baz" component={Baz} />
-                    </BarLayout>
-                  </Switch>
-                </Route>
-              </Switch>
-            </AnimatePresence>
-          )}
-        /> */}
       </div>
-    {/* </ConnectedRouter> */}
     </Router>
   );
 }
-
-// function Navigation() {
-//   return (
-//     <nav>
-//       <ul>
-//         <li>layout foo</li>
-//         <li>
-//           <Link
-//             to={{
-//               pathname: "/foo",
-//               state: { fromLayout: "foo" },
-//             }}
-//           >
-//             Foo
-//           </Link>
-//         </li>
-//         <li>layout bar</li>
-//         <li>
-//           <Link
-//             to={{
-//               pathname: "/bar",
-//               state: { fromLayout: "bar" },
-//             }}
-//           >
-//             Bar
-//           </Link>
-//         </li>
-//         <li>
-//           <Link
-//             to={{
-//               pathname: "/baz",
-//               state: { fromLayout: "bar" },
-//             }}
-//           >
-//             Baz
-//           </Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// }
 
 function Foo() {
   return <h2>Foo</h2>;
